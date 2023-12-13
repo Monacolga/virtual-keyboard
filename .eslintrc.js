92177c62aba309f3f9fd7a027bb897e4475047a5
Olga Pedashenko <olga.pedashenko@gmail.com>
2023-12-13 23:49:35 +0100

    feat: update

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,13 @@
 module.exports = {
   env: {
     browser: true,
-    commonjs: true,
     es2021: true,
   },
-  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
+  extends: ["eslint:recommended", "airbnb-base"],
   overrides: [],
-  parser: "@typescript-eslint/parser",
   parserOptions: {
     ecmaVersion: "latest",
+    sourceType: "module",
   },
-  plugins: ["@typescript-eslint"],
   rules: {},
 };
